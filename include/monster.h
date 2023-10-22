aadf484523f902822a8c7f8fb26172e0c777f304
Murielle1 <msanou1@myges.fr>
2023-10-22 16:11:13 +0200

    create monster structure and adding function for defense and attack

---
+++ include/monster.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct Monster {
+    float health;
+    int def;
+    int attack_min;
+    int attack_max;
+    int current_attack;
+    int type;
+    int exp_drop;
+} Monster;
+
+typedef struct Player {
+    float health;
+    int def;
+    int attack_min;
+    int attack_max;
+    int current_attack;
+    float money;
+    int exp;
+    int nb_arme;
+    int nb_armure;
+    int level;
+} Player;
+
+Monster *monster_attack(Monster *monster);
+Monster *monster_defense(Monster *monster, Player *ply);
+int get_reward();
+int get_reward_type();
+Monster *monster_death(Monster *monster, Player *ply);
