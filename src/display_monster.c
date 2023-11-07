20bc4b1356e1db47d9e1378b9b792a37d85104b8
jogoml <johanribl@gmail.com>
2023-11-07 17:40:29 +0100

    deplacer exemple

--- src/display_monster.c
+++ src/display_monster.c
@@ -13,8 +13,37 @@ char *m2[] = {
     "           |=|                     ",
     "          __|__                    ",
     "        //.=|=.0                   "};
+//######################################################
+//                       EXEMPLE                      
 
+// Fight *fight = malloc(sizeof(Fight));
+// fight->nbMonsters = 5;
+// fight->target = 1;
+// fight->monsters = malloc(sizeof(Monster) * fight->nbMonsters);
 
+// fight->monsters[0].health = 20;
+// fight->monsters[0].height = 5;
+// attributSpriteToMonster(&(fight->monsters[0]));
+
+// fight->monsters[1].health = 100;
+// fight->monsters[1].height = 5;
+// attributSpriteToMonster(&(fight->monsters[1]));
+
+// fight->monsters[2].health = 100;
+// fight->monsters[2].height = 5;
+// attributSpriteToMonster(&(fight->monsters[2]));
+
+// fight->monsters[3].health = 100;
+// fight->monsters[3].height = 5;
+// attributSpriteToMonster(&(fight->monsters[3]));
+
+// fight->monsters[4].health = 100;
+// fight->monsters[4].height = 5;
+// attributSpriteToMonster(&(fight->monsters[4]));
+
+// displayMonster(fight);
+
+//######################################################
 void attributSpriteToMonster(Monster * monster){
     monster->sprite = malloc(sizeof(char *) * monster->height);
     srand(time(NULL));
--- src/main.c
+++ src/main.c
@@ -3,32 +3,7 @@
 int main(int argc, char **argv)
 {
 
-    Fight *fight = malloc(sizeof(Fight));
-    fight->nbMonsters = 5;
-    fight->target = 1;
-    fight->monsters = malloc(sizeof(Monster) * fight->nbMonsters);
-
-    fight->monsters[0].health = 20;
-    fight->monsters[0].height = 5;
-    attributSpriteToMonster(&(fight->monsters[0]));
-
-    fight->monsters[1].health = 100;
-    fight->monsters[1].height = 5;
-    attributSpriteToMonster(&(fight->monsters[1]));
     
-    fight->monsters[2].health = 100;
-    fight->monsters[2].height = 5;
-    attributSpriteToMonster(&(fight->monsters[2]));
-
-    fight->monsters[3].health = 100;
-    fight->monsters[3].height = 5;
-    attributSpriteToMonster(&(fight->monsters[3]));
-    
-    fight->monsters[4].health = 100;
-    fight->monsters[4].height = 5;
-    attributSpriteToMonster(&(fight->monsters[4]));
-    
-    displayMonster(fight);
 
     // Context* context = malloc(sizeof(Context));
     // context->x = 3;
