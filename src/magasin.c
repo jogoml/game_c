77ec80a25e1ee8f10039c2f936ad675b7946407c
Murielle1 <msanou1@myges.fr>
2023-11-12 20:21:28 +0100

    correct display shop

--- src/magasin.c
+++ src/magasin.c
@@ -4,9 +4,10 @@ void display_inventory_weapon(Weapon **wp, Player *ply)
 {
     clearScreen();
     printf("Quelle arme voulez-vous acheter?\n");
+    printf("Argent restant: %.2f\n", ply->money);
     printf("0. Retour\n");
     for (int i = 0; i < 7; i++) {
-        if (ply->weapons[i] && strcmp(ply->weapons[i]->name,wp[i]->name) == 0)
+        if (ply->weapons[i] && strcmp(ply->weapons[i]->name, wp[i]->name) == 0)
             continue;
         printf("%d. Arme: %s\tAttaque Min: %d\tAttaque Max: %d\tNombre d'attaques par tour: %d\tPrix: %.2f\n",
         i+1, wp[i]->name, wp[i]->attaqueMin, wp[i]->attaqueMax, wp[i]->attaquesParTour, wp[i]->price);
@@ -16,6 +17,7 @@ void display_inventory_weapon(Weapon **wp, Player *ply)
 void display_inventory_armor(Armor **armor, Player *ply)
 {
     clearScreen();
+    printf("Argent restant: %.2f\n", ply->money);
     printf("Quelle armure voulez-vous acheter?\n");
     printf("0. Retour\n");
     for (int i = 0; i < 7; i++) {
@@ -29,6 +31,7 @@ void display_inventory_armor(Armor **armor, Player *ply)
 void display_inventory_spell(Spell **spell, Player *ply)
 {
     clearScreen();
+    printf("Argent restant: %.2f\n", ply->money);
     printf("Quel sort voulez-vous acheter?\n");
     printf("0. Retour\n");
     for (int i = 0; i < 9; i++) {
@@ -52,6 +55,7 @@ void magasin(Player *ply)
 {
     char c = '-';
     char choice = '-';
+    printf("Argent restant: %.2f\n", ply->money);
     while (choice != '1' && choice != '2' && choice  != '3' && choice != '0') {
         printf("Que voulez-vous acheter?\n1- Une arme\n2- Une armure\n3- Un sort\n0- Continuer\n");
         system ("/bin/stty raw");
@@ -73,8 +77,11 @@ void magasin(Player *ply)
             ply->money -= wp[(c -48) -1]->price;
             ply->weapons = add_weapon(ply->weapons, wp[(c -48) - 1], ply->nb_arme);
             ply->nb_arme++;
+            printf("Arme %s achetée !\n", wp[(c -48) - 1]->name);
+            save_weapon(ply);
             magasin(ply);
-        }
+        } else
+            printf("Vous n'avez pas assez d'argent pour acheter cette arme!\n");
     } else if (choice == '2') {
         Armor **armor = inventory_armor();
         display_inventory_armor(armor, ply);
@@ -91,8 +98,11 @@ void magasin(Player *ply)
             ply->money -= armor[(c -48) -1]->price;
             ply->armors = add_armor(ply->armors, armor[(c -48) - 1], ply->nb_armure);
             ply->nb_armure++;
+            printf("Armure %s achetée !\n", armor[(c -48) - 1]->name);
+            save_armor(ply);
             magasin(ply);
-        }
+        } else
+            printf("Vous n'avez pas assez d'argent pour acheter cette armure !\n");
     } else if (choice == '3') {
         Spell **spell = inventory_spell();
         display_inventory_spell(spell, ply);
@@ -108,8 +118,11 @@ void magasin(Player *ply)
             ply->money -= spell[(c -48) -1]->price;
             ply->spell = add_spell(ply->spell, spell[(c -48) - 1], ply->nb_spell);
             ply->nb_spell++;
+            printf("Sort %s acheté !\n", spell[(c -48) - 1]->name);
+            save_spell(ply);
             magasin(ply);
-        }
+        } else
+            printf("Vous n'avez pas assez d'argent pour acheter cette potion !\n");
     } else if (choice == '0') {
         save_armor(ply);
         save_weapon(ply);
